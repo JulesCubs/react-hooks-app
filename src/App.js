a5eebb393bd5127396c66c4b89480cd9c406ebe0
Julian Cubillos <juliandavid25@hotmail.com>
2021-04-16 14:24:19 -0500

    Uso de useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"; //Esta linea ya no es obligatoria react puede reconocer sin este cambio
 import Header from "./components/Header";
+import Characters from "./components/Characters";
 import "./App.css";
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<Characters />
 			<h1>Hola React Hooks</h1>
 		</div>
 	);
